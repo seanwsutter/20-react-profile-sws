87520926434b9eadfcfeecf3a01f9d693b7c8419
seanwsutter <seanwsutter@gmail.com>
2023-12-14 16:45:54 -0600

    remove some vite install files, added more file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
 import { useState } from 'react'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import components
+import Header from './components/Header';
+import Aboutme from './components/Aboutme';
+import Footer from './components/Footer';
+// import Header from './components/Header'
+// import Header from './components/Header'
+
+// css
+// import './App.css';
+
+
+export default function App() {
 
   return (
     <>
-    </>
+      <Header />
+      <BrowserRouter>
+        <Routes>
 
+          <Route to="/" element={<Aboutme />} />
+          {/* <Route to="">Portfolio</Route>
+          <Route to="">Contact</Route>
+          <Route to="">Resume</Route> */}
+
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
-export default App
 
 
-{/* <>
-<div>
-  <a href="https://vitejs.dev" target="_blank">
-    <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a>
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
-</div>
-<h1>Vite + React</h1>
-<div className="card">
-  <button onClick={() => setCount((count) => count + 1)}>
-    count is {count}
-  </button>
-  <p>
-    Edit <code>src/App.jsx</code> and save to test HMR
-  </p>
-</div>
-<p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-</p>
-</> */}
+{/*   return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+  
+            <Navbar.Link href="">About </Navbar.Link>
+            <Navbar.Link href="">Portfolio</Navbar.Link>
+            <Navbar.Link href="">Contact</Navbar.Link>
+            <Navbar.Link href="">Resume</Navbar.Link>
+    
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )*/}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
+
 export default function Header() {
   return (
-    <header>
 
-    </header>
+    <Navbar className="bg-dark">
+      <Container>
+        <Nav>
+          <Navbar.Link href="">About </Navbar.Link>
+          <Navbar.Link href="">Portfolio</Navbar.Link>
+          <Navbar.Link href="">Contact</Navbar.Link>
+          <Navbar.Link href="">Resume</Navbar.Link>
+        </Nav >
+      </Container >
 
-  )
-}
+    </Navbar>
+  );
+};
